8d3f5189825692e7719addd2d920d057247ba739
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-21 19:02:05 -0500

    TUnit conversion

--- test/Soenneker.Extensions.String.Html.Tests/HtmlStringExtensionTests.cs
+++ test/Soenneker.Extensions.String.Html.Tests/HtmlStringExtensionTests.cs
@@ -1,11 +1,10 @@
 using Soenneker.Tests.Unit;
-using Xunit;
 
 namespace Soenneker.Extensions.String.Html.Tests;
 
 public class HtmlStringExtensionTests : UnitTest
 {
-    [Fact]
+    [Test]
     public void Default()
     {
 
